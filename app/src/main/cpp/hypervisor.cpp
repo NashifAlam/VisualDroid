5e8612928756f343ef20a8d45537dbad754521ae
NashifAlam <nashifalamscar@gmail.com>
2023-02-19 22:04:45 +0530

    Added C++ functionality

--- app/src/main/cpp/hypervisor.cpp
+++ app/src/main/cpp/hypervisor.cpp
@@ -15,3 +15,18 @@
 //         System.loadLibrary("hypervisor")
 //      }
 //    }
+
+class preprocessor{
+    private:
+        int initString = 100000000;
+        int TyreString = 100000000, BrakeString = 100000000, DoorString = 100000000, ToggleString = 100000000, DiagnosticString = 100000000;
+
+    public:
+        int rxParser(int Tyre, int Brake, int Door, int Toggle, int Diagnostics);
+        void valueTyre(int Tyre);
+        void valueBrake(int Brake);
+        void valueDoor(int Door);
+        void valueToggle(int Toggle);
+        void valueDiagnostics(int Toggle);
+
+};
